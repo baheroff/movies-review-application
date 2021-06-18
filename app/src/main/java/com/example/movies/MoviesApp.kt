0e51fbd8f3b4e2faa368532c41b26b7adb61dd3b
baheroff <danikdsbaherov@gmail.com>
2021-06-18 18:51:41 +0300

    fix repo

--- app/src/main/java/com/example/movies/MoviesApp.kt
+++ app/src/main/java/com/example/movies/MoviesApp.kt
@@ -3,13 +3,14 @@ package com.example.movies
 import android.app.Application
 import com.example.movies.database.MoviesDatabase
 import com.example.movies.database.MoviesRepository
+import com.example.movies.network.NetworkModule
 
 class MoviesApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
         val database = MoviesDatabase.getDatabase(applicationContext)
-        repository = MoviesRepository(database)
+        repository = MoviesRepository(database, NetworkModule())
     }
 
     companion object {
